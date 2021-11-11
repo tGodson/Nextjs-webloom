647c0b6637ee4716e23bad191ff5d15bba8108d0
tGodson <tendongze95@gmail.com>
2021-11-11 23:32:22 +0100

    initinalize nextjs app

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -14,6 +15,13 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+        
+        <h1 className="title">
+          Read{' '}
+          <Link href="/teams/team">
+            <a>this page!</a>
+          </Link>
         </h1>
 
         <p className={styles.description}>
